aa19b825ef2e6d53f433c41e11d0283483293f80
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2023-12-19 18:33:27 +0200

    asdsa

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -44,9 +44,9 @@ export default function Navbar() {
             </nav>
             {toggle && ( 
                 <div className="absolute top-20 z-50  w-full min-h-[30vh] dark:bg-[#013240] bg-[#EFF0F2] animate-fade-down">
-                    <Link href="/" onClick={closeMenu} className="min-h-[10vh] grid items-center justify-center dark:text-[#FFE9B7] text-[#54BA6E] hover:font-extrabold hover:italic hover:text-[#54BA6E]">Home</Link>
-                    <Link href="/work/ahmad-elmesery-portfolio" onClick={closeMenu} className="min-h-[10vh] grid items-center justify-center dark:text-[#FFE9B7] text-[#54BA6E] hover:font-extrabold hover:italic hover:text-[#54BA6E]">Work</Link>
-                    <Link href="/about" onClick={closeMenu} className="min-h-[10vh] grid items-center justify-center dark:text-[#FFE9B7] text-[#54BA6E] hover:font-extrabold hover:italic hover:text-[#54BA6E]">About</Link>
+                    <Link href="/" onClick={closeMenu} className="min-h-[10vh] border-b-[0.1px] dark:border-gray-700 grid items-center justify-center dark:text-[#FFE9B7] text-[#54BA6E] hover:font-extrabold hover:italic hover:text-[#54BA6E]">Home</Link>
+                    <Link href="/work/ahmad-elmesery-portfolio" onClick={closeMenu} className="min-h-[10vh] border-b-[0.1px] dark:border-gray-700 grid items-center justify-center dark:text-[#FFE9B7] text-[#54BA6E] hover:font-extrabold hover:italic hover:text-[#54BA6E]">Work</Link>
+                    <Link href="/about" onClick={closeMenu} className="min-h-[10vh] border-b-[0.1px] dark:border-gray-700 grid items-center justify-center dark:text-[#FFE9B7] text-[#54BA6E] hover:font-extrabold hover:italic hover:text-[#54BA6E]">About</Link>
                 </div>
             )}
         </>
