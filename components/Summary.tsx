cee62892d64cc50506e6fe433cc767f179d80786
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2023-12-19 03:43:55 +0200

    sdsad

--- components/Summary.tsx
+++ components/Summary.tsx
@@ -1,10 +1,18 @@
 import { PortableText } from "@portabletext/react";
+import {
+  FaBehance,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaWhatsapp,
+  FaYoutube,
+} from "react-icons/fa";
 import { getProfile } from "../sanity/sanity-utils.ts";
 
 export const revalidate = 10;
 
 export default async function Summary() {
-  const { summary, email } = await getProfile();
+  const { summary, email, linkedin, facebook, behance, instagram, youtube, whatsapp } = await getProfile();
   return (
     <>
       <PortableText value={summary} /> <br />
@@ -12,6 +20,42 @@ export default async function Summary() {
         Feel free to ask questions or contact for possible business inquiries{" "}
         {email}
       </p>
+      <div className="flex flex-row items-center mt-4git  gap-3 sm:gap-5">
+        {linkedin && (
+          <a href={linkedin} target="_blank" title={linkedin}>
+            <FaLinkedinIn />
+          </a>
+        )}
+        {facebook && (
+          <a href={facebook} target="_blank" title={facebook}>
+            <FaFacebookF />
+          </a>
+        )}
+        {behance && (
+          <a href={behance} target="_blank" title={behance}>
+            <FaBehance />
+          </a>
+        )}
+        {youtube && (
+          <a href={youtube} target="_blank" title={youtube}>
+            <FaYoutube />
+          </a>
+        )}
+        {whatsapp && (
+          <a
+            href={`https://wa.me/2${whatsapp}`}
+            target="_blank"
+            title={whatsapp}
+          >
+            <FaWhatsapp />
+          </a>
+        )}
+        {instagram && (
+          <a href={instagram} target="_blank" title={instagram}>
+            <FaInstagram />
+          </a>
+        )}
+      </div>
     </>
   );
 }
