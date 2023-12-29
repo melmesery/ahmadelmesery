5a4f69e8c24b6f44883e585e9ffc7517c3f09afe
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2023-12-29 10:19:09 +0200

    lkl

--- components/Images.tsx
+++ components/Images.tsx
@@ -24,48 +24,46 @@ const Images = ({ project }: any) => {
     >
       {show && (
         <>
-          {/* <div className="grid grid-col-1 sm:grid-cols-2 gap-1 mb-1 items-center justify-between"> */}
-          <div className="grid xs:grid-cols-2 grid-cols-1 gap-1 mb-1 items-center justify-between">
+           <div className="grid xs:grid-cols-2 grid-cols-1 gap-1 mb-1 items-center justify-between">
             <Image
               src={mediumImages[0]}
               alt={name}
               width={650}
               height={340}
-              className="animate-fade-up"
+              className="animate-fade-up animate-delay-500 animate-ease-in-out"
             />
             <Image
               src={mediumImages[1]}
               alt={name}
               width={650}
               height={340}
-              className="animate-fade-up"
+              className="animate-fade-up animate-delay-500 animate-ease-in-out"
             />
           </div>
-          {/* <div className="grid grid-col-1 sm:grid-cols-3 gap-1 mb-1 items-center justify-between"> */}
-          <div className="grid xs:grid-cols-3 grid-cols-1 gap-1 mb-1 items-center justify-between">
+           <div className="grid xs:grid-cols-3 grid-cols-1 gap-1 mb-1 items-center justify-between">
             <Image
               src={smallImages[0]}
               alt={name}
               width={500}
               height={230}
-              className="animate-fade-up"
+              className="animate-fade-up animate-delay-[2000ms] animate-ease-in-out"
             />
             <Image
               src={smallImages[1]}
               alt={name}
               width={500}
               height={230}
-              className="animate-fade-up"
+              className="animate-fade-up animate-delay-[2000ms] animate-ease-in-out"
             />
             <Image
               src={smallImages[2]}
               alt={name}
               width={500}
               height={230}
-              className="animate-fade-up"
+              className="animate-fade-up animate-delay-[1500ms] animate-ease-in-out"
             />
           </div>
-          <div className="animate-fade-up">
+          <div className="animate-fade-up animate-delay-[3000ms] animate-ease-in-out">
             <Image src={bigImage} alt={name} width={1260} height={690} />
           </div>
         </>
