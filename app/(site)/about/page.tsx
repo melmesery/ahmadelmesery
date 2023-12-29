cba8aedc123419072210b747d50ba2b7e6c21499
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2023-12-29 10:04:37 +0200

    lk

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -7,10 +7,10 @@ export default function About() {
     <div className="about_container">
       <h1 className="about_headline">About</h1>
       <div className="about_data">
-        <div className="about_profile">
+        <div className="about_profile animate-fade-up">
           <Profile />
         </div>
-        <div className="about_summary">
+        <div className="about_summary animate-fade-up">
           <Summary />
         </div>
       </div>
