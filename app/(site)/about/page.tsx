46d2765ba2e479090954cb7723520f720dde3df3
Mohamed El Mesery <107087703+melmesery@users.noreply.github.com>
2024-02-11 01:15:15 +0200

    sdas

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -1,23 +1,27 @@
-"use client";
+// "use client";
 
 import Profile from "@/components/Profile.tsx";
 import Summary from "@/components/Summary.tsx";
 import "@/styles/About.css";
-import AOS from "aos";
-import { useEffect } from "react";
+// import AOS from "aos";
+// import { useEffect } from "react";
 
 export default function About() {
-  useEffect(() => {
-    AOS.init();
-  }, []);
+  // useEffect(() => {
+  //   AOS.init();
+  // }, []);
 
   return (
     <div className="about_container">
       <div className="about_data">
-        <div className="about_profile" data-aos="flip-up">
+        <div className="about_profile" 
+        // data-aos="flip-up"
+        >
           <Profile />
         </div>
-        <div className="about_summary" data-aos="fade-up">
+        <div className="about_summary" 
+        // data-aos="fade-up"
+        >
           <Summary />
         </div>
       </div>
